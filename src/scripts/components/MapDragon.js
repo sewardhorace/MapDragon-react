ba0350013a1fbfd137b1e2a3489d6733d4564bdf
Max White <maxjwhite22@gmail.com>
2016-01-26 19:54:18 -0500

    showing/hiding popup on double-click

--- src/scripts/components/MapDragon.js
+++ src/scripts/components/MapDragon.js
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+//helpers
 var mousePosition = require('../classes/helpers').relativeMousePos;
+
+//components
+var Popup = require('./Popup');
 
 var MapDragon = React.createClass({
   getInitialState: function(){
@@ -11,7 +16,7 @@ var MapDragon = React.createClass({
   componentDidMount: function() {
     var context = this.refs.canvas.getContext('2d');
     this.setState({
-      context: context
+      context: context,
     });
   },
   getMouse: function(e){
@@ -21,8 +26,9 @@ var MapDragon = React.createClass({
     return mouse;
   },
   handleDoubleClick: function(e){
-    e.preventDefault();
-    this.getMouse(e);
+    // e.preventDefault();//is this necessary?  need to prevent text highlighting
+    var m = mousePosition(e);
+    this.refs.popup.show(m.x, m.y);
   },
   render: function() {
     return (
@@ -30,6 +36,7 @@ var MapDragon = React.createClass({
         <h2>double-click to add icons to the canvas</h2>
         <div id="canvas-container">
           <canvas onDoubleClick={this.handleDoubleClick} width="500" height="500" ref="canvas" />
+          <Popup ref="popup"/>
         </div>
       </div>
     );
--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,25 +2,65 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Popup = React.createClass({
+  getInitialState: function(){
+    var spriteSheet = new Image();
+    spriteSheet.src = 'static/assets/images/cowboyspritestrip.png';
+    return {
+      options:[],
+      selectedOptionIdx: -1,
+      x: 0,
+      y: 0,
+      visible: false
+    };
+  },
+  componentDidMount: function(){
+
+  },
+  componentDidUpdate: function(prevProps, prevState){
+    if (this.state.visible === true){
+      this.refs.steadingNameInput.focus();
+    }
+  },
+  //should "show" & "hide" live in the mapdragon component?
+  show: function(x, y){
+    this.setState({
+      x: x,
+      y: y,
+      visible: true
+    });
+  },
+  hide: function(){
+    this.setState({
+      visible: false
+    });
+  },
   render: function() {
-    return (
-      <div id="popup" className="hidden">
-        <div className="popup-nav">
-          <input className="nav-title nav-left" placeholder="Title (optional)">
-          <div className="nav-item nav-right">x</div>
-          <div className="nav-item nav-right">+</div>
+    var popupStyle = {
+      left: this.state.x,
+      top: this.state.y
+    };
+    if (this.state.visible) {
+      return (
+        <div id="popup" style={popupStyle}>
+          <div className="popup-nav">
+            <input ref="steadingNameInput" className="nav-title nav-left" placeholder="Title (optional)"/>
+            <div className="nav-item nav-right">x</div>
+            <div className="nav-item nav-right">+</div>
+          </div>
+          <div className="popup-window">
+            <ul className="popup-options">
+              <li className="popup-item">
+                <div className="popup-icon-frame">
+                  <img className="popup-icon-image"/>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="popup-window">
-          <ul className="popup-options">
-            <li className="popup-item">
-              <div className="popup-icon-frame">
-                <img className="popup-icon-image">
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return null
+    }
   }
 });
 
